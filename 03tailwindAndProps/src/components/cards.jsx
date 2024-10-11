b4574be5c45fb1c0aaf42135626c358e110d5cc3
Saurabh Vaidya <vaidyasaurabh48@gmail.com>
2024-10-12 02:46:02 +0530

    Tailwind and props

---
+++ 03tailwindAndProps/src/components/cards.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Card } from 'antd';
+const { Meta } = Card;
+
+function Cards({username}) {
+    return (
+        <Card
+            hoverable
+            style={{
+                width: 240,
+            }}
+            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+        >
+            <Meta title={username} description="www.instagram.com" />
+        </Card>
+    )
+}
+
+export default Cards
